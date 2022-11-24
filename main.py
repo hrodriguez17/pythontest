da0942c4bfcbf5a88380a4e1dbfbcdf0235dcaf5
hrodriguez17 <117259033+hrodriguez17@users.noreply.github.com>
2022-11-24 05:35:02 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,50 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
+import time
+
+
+def get_game(name, df):
+    print('get game running')
+    dfy = df.loc[df['Name'] == name]
+    game_id = dfy['id'].iloc[0]
+    return game_id
+
+
+def run_rec(game_id):
+    with st.spinner('Wait for it...'):
+        cm = CountVectorizer().fit_transform(game_data['Summary'].values.astype('U'))
+
+        cs = cosine_similarity(cm)
+
+        game = game_data['Name'][game_id]
+
+        game_id = game_data[game_data.Name == game]['id'].values[0]
+
+        scores = list(enumerate(cs[game_id]))
+
+        sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
+        sorted_scores = sorted_scores[1:]
+        j = 0
+        st.write("Here are your recommendations for: " + str(game))
+        for item in sorted_scores:
+            game_title = game_data[game_data.id == item[0]]['Name'].values[0]
+            x = str(j + 1) + ': ' + str(game_title)
+            st.write(x)
+            st.write('  Similarity: ' + str(item[1]))
+            j = j + 1
+            if j >= 5:
+                break
+
 
 game_data = pd.read_csv('file2.csv', encoding="utf-8", sep=';')
 
-columns = ['Name', 'Summary']
+col_one_list = game_data['Name'].tolist()
+sb_01 = st.selectbox('Select', col_one_list)
+idf = get_game(sb_01, game_data)
+print('printing', idf)
 
-cm = CountVectorizer().fit_transform(game_data['Summary'].values.astype('U'))
+if st.button("Recommend Me!"):
+    run_rec(get_game(sb_01, game_data))
 
-cs = cosine_similarity(cm)
-
-game = game_data['Name'][2]
-# print(game)
-
-game_id = game_data[game_data.Name == game]['id'].values[0]
-print(game_id)
-
-scores = list(enumerate(cs[game_id]))
-sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
-sorted_scores = sorted_scores[1:]
-j = 0
-print("recommend for " + game)
-for item in sorted_scores:
-    game_title = game_data[game_data.id == item[0]]['Name'].values[0]
-    print(j + 1, game_title)
-    j = j + 1
-    if j >= 5:
-        break
