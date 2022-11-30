d4cf51b2d02a40712f70106886ea23133a13813c
hrodriguez17 <117259033+hrodriguez17@users.noreply.github.com>
2022-11-30 04:33:56 -0700

    Delete json_to_csv_sep.py

--- json_to_csv_sep.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-df = pd.read_csv('file.csv', encoding="utf-8", sep=';', nrows=10000, index_col=False)
-ti_col = ['Name', 'Short Description', 'Developer', 'Publisher', 'Genre',
-          'Tags', 'Categories', 'Positive Reviews', 'Negative Reviews']
-df = df.iloc[0]
-dff = df.join(pd.DataFrame(columns=ti_col))
